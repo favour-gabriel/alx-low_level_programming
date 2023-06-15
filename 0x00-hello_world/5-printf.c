83509939e9c16cbcf9dc94c864575e2143ba8e53
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-15 05:43:12 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Proper grammer
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art, \n");
+		return (0);
+}
