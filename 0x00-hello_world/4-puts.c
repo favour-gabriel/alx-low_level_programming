f5dd5b36d508ab69d3f18445fc14e46c4f0f717d
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-15 04:50:18 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /** 
  * main - Entry point
- * Return: Always 0 (Sucess)
- * */
-
+ * Return: 0 (Sucess)
+  */
 int main (void)
 {
-	puts("\"Programming is like buliding a multilingual puzzle");
-
-	return(0);
-
+puts("\"Programming is like buliding a multilingual puzzle");
+return(0);
 }
