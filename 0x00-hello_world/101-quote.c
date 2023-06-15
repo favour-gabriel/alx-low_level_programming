f338a65daa3895575ffaf02f98c1e207c21fc501
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-15 07:17:18 -0700

    Quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints UNIX
+ * main - UNIX
  * Return: 1 (Success)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-1 0-19\n", stdout);
 return (1);
 }
