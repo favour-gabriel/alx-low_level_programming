736a6588fd2a1eee2b1e21e901a1cfdde0885dbc
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-16 10:39:35 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,10 +10,15 @@ int main(void)
 	int p, q;
 
 	for (p = 0; p <= 98; p++)
-	for (q = p + 1; q <= 99; q++)
+	{
+		for (q = p + 1; q <= 99; q++)
+		{
 	putchar((p / 10) + '0');
 putchar((p % 10) + '0');
+putchar(',');
 putchar(' ');
+		}
+	}
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
 if (p == 98 && q == 99)
