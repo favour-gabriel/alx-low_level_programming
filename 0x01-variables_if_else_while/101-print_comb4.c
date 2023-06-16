f011f7469f0769a010edb3cee816cb40d93cbf2b
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-16 10:23:09 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,25 +7,28 @@
  */
 int main(void)
 {
-
-	int d, p;
+	int d, p, q;
 
 	for (d = '0'; d < '9'; d++)
 	{
 
 		for (p = d + 1; p <= '9'; p++)
 		{
-if (p != d)
-{
-	putchar(d);
-	putchar(p);
-	if (d == '8' && p == '9')
-	continue;
-	putchar(',');
+			for (q = p + 1; q <= '9'; q++)
+			{
+				if ((p != d) != q)
+				{
+			putchar(d);
+			putchar(p);
+			putchar(q);
+			if (d == '7' && p == '8')
+			continue;
+		putchar(',');
 	putchar(' ');
-}
-}
-}
+				}
+			}
+		}
+	}
 putchar('\n');
 return (0);
 }
