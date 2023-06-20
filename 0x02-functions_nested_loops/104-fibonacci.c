e492f579aea0da5a8af94fe61110de8a4d318223
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-20 08:26:42 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -24,6 +24,10 @@ fib1_half2 = fib1 % 10000000000;
 fib2_half2 = fib2 % 10000000000;
 for (count = 91; count < 99; count++)
 {
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
 half1 += 1;
 half2 %= 10000000000;
 }
