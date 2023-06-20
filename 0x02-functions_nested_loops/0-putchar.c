9564a42c37ee68da9444c7d4ab2e2ae38489691b
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-20 03:40:58 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: 0
+ * main - Entry Point
+ * Return: Always 0
  */
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
