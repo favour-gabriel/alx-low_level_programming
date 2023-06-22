67139344e1b1754e1cf8021f0bbdf28a875985de
favour-gabriel <favourgabrielkennedy@gmail.com>
2023-06-22 15:53:43 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,15 +12,13 @@ int x, y;
 if (n <= 0)
 {
 _putchar('\n');
-}
-else
-{
 for (x = 0; x < n; x++)
 {
 for (y = 0; y < x; y++)
 {
-_putchar(32);
-_putchar('\n');
+_putchar(' ');
 }
+_putchar('\\');
+_putchar('\n');
 }
 }
