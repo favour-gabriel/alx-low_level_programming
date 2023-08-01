3869e68c3dc4499ac3ced938a4a79bf0c04462a4
favour-gabriel <favougabrielkennedy@gmail.com>
2023-08-01 01:26:56 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * print_listint - It should print all the linked list.
- * @h: Type listint_t ro print.
+ * @h: Type listint_t to print.
  * Return: The numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
