017307d836f732abed1d2e8a559c0772cea91661
favour-gabriel <favougabrielkennedy@gmail.com>
2023-08-03 08:59:16 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,20 +5,20 @@
  */
 void print_binary(unsigned long int n)
 {
-	int t, cut = 0;
-	unsigned long int cent;
+int t, cut = 0;
+unsigned long int cent;
 
-	for (t = 63; t >= 0; t--)
-	{
-		cent = n >> t;
-			if (cent & 1)
-		{
-			_putchar('1');
-			cut++;
-		}
-		else if (cut)
-			_putchar('0');
-	}
-	if (!cut)
-		_putchar('0');
+for (t = 63; t >= 0; t--)
+{
+cent = n >> t;
+if (cent & 1)
+{
+_putchar('1');
+cut++;
+}
+else if (cut)
+_putchar('0');
+}
+if (!cut)
+_putchar('0');
 }
